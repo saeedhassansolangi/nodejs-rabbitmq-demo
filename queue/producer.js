46c65d9ce846047aa1e139edd3b43857b2235139
saeed.hassan <saeed.hassan@paysyslabs.com>
2024-08-02 11:49:19 +0500

    updated

--- queue/producer.js
+++ queue/producer.js
@@ -1,16 +1,33 @@
 const rabbitMQConnection = require('../rabbitmq_connection');
 const users = require('../users');
 
-rabbitMQConnection()
-.then((channel) => {   
 
-    for (let i = 0; i < 10; i++) {
-        setTimeout(() => {
-            console.log('Message sent to the queue: ', users[i]);
-            channel.sendToQueue('node-queue', Buffer.from(JSON.stringify(users[i])));
-        }, 2000 * i);
-    }
+const EXCHANGE = "java-exchange";
+
+const TEXT_ROUTING_KEY = "java-queue-binding-exchange";
+const JSON_ROUTING_KEY = "json_java_queue_binding_exchange";
+const TEXT_QUEUE = "java-queue";
+const JSON_QUEUE = "json_java_queue";
+
+rabbitMQConnection()
+.then(async (channel) => {   
+    await channel.assertExchange(EXCHANGE);
+
+    await channel.assertQueue(TEXT_QUEUE);
+    await channel.assertQueue(JSON_QUEUE);
     
+    await channel.bindQueue(TEXT_QUEUE, EXCHANGE, TEXT_ROUTING_KEY);
+    await channel.bindQueue(JSON_QUEUE, EXCHANGE, JSON_ROUTING_KEY);
+
+    channel.publish(EXCHANGE, TEXT_ROUTING_KEY, Buffer.from("Hello from Node.js in text format"), { persistent: true });
+    console.log('Message published to java-queue-binding-exchange');
+
+    let userAtIndex =  Math.floor(Math.random() * users.length);
+    let user = users[userAtIndex];
+    console.log("sending user with index:", userAtIndex, ", user: ", user);
+    channel.publish(EXCHANGE, JSON_ROUTING_KEY, Buffer.from(JSON.stringify(user)), { persistent: true });
+    console.log('Message published to json_java_queue_binding_exchange');
+
 })
 .catch((error)=>{
         console.log('Error in RabbitMQ connection: ', error);
